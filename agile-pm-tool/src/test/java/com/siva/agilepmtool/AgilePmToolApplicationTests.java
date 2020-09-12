9a3850e0ccbcb29d53ea80a72b32fe2a6e588e85
sambujanni <siva.janni11@gmail.com>
2020-09-13 02:16:02 +0530

    first commit

---
+++ agile-pm-tool/src/test/java/com/siva/agilepmtool/AgilePmToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siva.agilepmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgilePmToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
